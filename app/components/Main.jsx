de55ba886ca9d75d3d655e5a4852a48a81febd64
Warner Nielsen <bwn888@gmail.com>
2017-06-10 20:57:23 -0600

    Prepare files to be more biolerplate

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,12 +1,11 @@
 var React = require('react');
-var Nav = require('Nav');
 
 var Main = (props) => {
 	return (
 		<div>
-			<Nav/>
-			<div className="row">
-				<div className="columns medium-6 large-4 small-centered">
+			<div>
+				<div>
+					<p>Main.jsx rendered</p>
 					{props.children}
 				</div>
 			</div>
